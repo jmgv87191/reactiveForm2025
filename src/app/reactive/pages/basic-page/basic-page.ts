488e93fc861778e1f77e3a37a9aebcdaf273b863
jmgv87191 <jmgv87191@gmail.com>
2025-08-20 17:04:24 -0700

    asd

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -20,11 +20,18 @@ export class BasicPage {
 
 
   onSave(){
-    this.myForm.markAllAsTouched()
 
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched()
+      return;
+    }
+
+    
     console.log( this.myForm.value )
+    
+      
+      this.myForm.reset()
 
-    this.myForm.reset()
   }
 
 
